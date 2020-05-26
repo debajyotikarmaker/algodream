1cde0b03c15da1e0a4e108249961cdaad1e1915e
debajyotikarmaker <uqdkarma@gmail.com>
2020-05-26 22:36:16 +0600

    Updates

---
+++ precache-manifest.8eb9107a5c1ebc129fab653341e7fba7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a43d4d28d07c1768ed51d473be48ee5",
+    "url": "/algodream/index.html"
+  },
+  {
+    "revision": "e81e1b7b49bce0611105",
+    "url": "/algodream/static/css/2.62c23a74.chunk.css"
+  },
+  {
+    "revision": "eba682d2029ddccf3ccb",
+    "url": "/algodream/static/css/main.ffa6a9cf.chunk.css"
+  },
+  {
+    "revision": "e81e1b7b49bce0611105",
+    "url": "/algodream/static/js/2.6a747538.chunk.js"
+  },
+  {
+    "revision": "86bf7d79e2d95c812ddd8e211e299cab",
+    "url": "/algodream/static/js/2.6a747538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba682d2029ddccf3ccb",
+    "url": "/algodream/static/js/main.16c346f0.chunk.js"
+  },
+  {
+    "revision": "6e722bbcc03b923b46b9",
+    "url": "/algodream/static/js/runtime-main.a5cc4db7.js"
+  }
+]);
